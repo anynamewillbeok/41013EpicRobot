5b48baf068ad22c9b959d287a14a6e4d5f7560e0
Michael <michael.j.curtis-1@student.uts.edu.au>
2024-10-16 10:20:09 +1100

    removing obselete code

--- GenericRenderable.m
+++ GenericRenderable.m
@@ -18,22 +18,6 @@ classdef (Abstract) GenericRenderable < handle & ParentChild & Tickable
             [self.tri, self.pts] = plyread(ply_file, 'tri');
             
         end
-        %% Set a pose and scale it ONLY FOR TROUBLESHOOTING
-        function position = set_brick_pose(self, x, y, z, rotation_matrix)
-
-            % Scale the Renderable
-            scale_factor = 0.25;
-            scaled_pts = self.pts*scale_factor;
-
-            % Set the fin position considering translation and rotation
-            translation_vector = [x, y, z];
-
-            % Apply rotation to the original points
-            rotated_pts = (rotation_matrix * scaled_pts')';
-
-            % Translate the rotated points
-            position = rotated_pts + repmat(translation_vector, size(rotated_pts, 1), 1);
-        end
         %% Set transform for object
         function set_transform_4by4(self, matrix)
 
