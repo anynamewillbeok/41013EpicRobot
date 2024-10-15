60ff3be2a03ceb6285d4ff736fb7f63f6aa71eeb
Michael Curtis <michael.curtis181@gmail.com>
2024-10-15 15:24:18 +1100

    Generic Renderable

--- testing_script.m
+++ testing_script.m
@@ -1,3 +1,3 @@
 test_plot = GenericRenderable('Bench.ply');
-test_plot_pos = test_plot.set_brick_pose(-1.6, 0.8, -0.2, eye(3));
-test_plot_patch = patch('Faces',test_plot.tri,'Vertices',test_plot_pos, 'FaceColor', [0 0.1 0.3], 'EdgeColor', 'none');
+test_plot.set_transform_4by4(trotx(deg2rad(90)));
+%test_plot.render()
