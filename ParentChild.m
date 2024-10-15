634a97a1c7650196467cfceb1a5a425b76ada827
James Oliver Hart <james.oliver.hart@gmail.com>
2024-10-15 18:47:15 +1100

    further refinement of access specifiers for ParentChild

--- ParentChild.m
+++ ParentChild.m
@@ -8,20 +8,34 @@ classdef (Abstract) ParentChild < handle
         %% Attach new parent
         function attach_parent(self, new_parent)
             if ~isempty(self.attached_parent{1})
-                self.attached_parent{1}.detach_child(self) 
+                self.attached_parent{1}.detach_child(self)
             end
 
             self.attached_parent{1} = new_parent;
             self.attached_parent{1}.attach_child(self)
         end
+
+        %% Make Orphan (attach current parent)
+        function orphan(self)
+
+            if ~isempty(self.attached_parent{1})
+                self.attached_parent{1}.detach_child(self)
+            end
+            self.attached_parent{1} = [];
+        end
+    end
+    methods(Access = private)
+        %These are locked away as private functions as they are used by
+        %attach_parent and orphan. Calling these directly would cause an
+        %inconsistent state.
         %% Detach
         function detach_child(self, object_to_detach)
             if ~isempty(self.attached_child)
                 i = 1;
                 while all(i >= 1 & i <= length(self.attached_child))
                     if self.attached_child{i} == object_to_detach
-                        self.attached_child(i) = []; 
-                        i = i - 1; 
+                        self.attached_child(i) = [];
+                        i = i - 1;
                     end
                     i = i + 1;
                 end
@@ -29,17 +43,7 @@ classdef (Abstract) ParentChild < handle
         end
         %% Attach Child
         function attach_child(self, child)
-           self.attached_child{end+1} = child; %curly braces here needed for cell array
-
-        end
-        %% Make Orphan
-        function orphan(self)
-
-            if ~isempty(self.attached_parent{1})
-                self.attached_parent{1}.detach_child(self)
-            end
-            self.attached_parent{1} = [];
-            
+            self.attached_child{end+1} = child; %curly braces here needed for cell array
         end
     end
 end
