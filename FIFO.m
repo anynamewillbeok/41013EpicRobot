0abb04c68e171ee4ee735d8fee81815d8c9e8d58
Michael <michael.j.curtis-1@student.uts.edu.au>
2024-10-23 10:04:17 +1100

    add FIFO

---
+++ FIFO.m
@@ -0,0 +1,28 @@
+classdef FIFO < handle
+    properties(SetAccess = private)
+        active(1,:)
+        queue(:,:)
+    end
+
+    methods
+        function self = FIFO(width)
+            active = createArray(1,width,'double');
+            queue = createArray(0,width,'double');
+        end
+
+        function vector = pull(self)
+            vector = self.active;
+            if ~isempty(self.queue)
+            self.active = self.queue(1,:);
+            self.queue(1,:) = [];
+            end
+        end
+
+        function add(self,matrix)
+            self.queue = vertcat(self.queue,matrix);
+            if isempty(self.active)
+                self.pull()
+            end
+        end
+    end
+end
