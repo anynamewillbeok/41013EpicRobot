f7e6f3b64389d782e89af87d0419ea76a54c0af7
James Oliver Hart <james.oliver.hart@gmail.com>
2024-10-22 13:42:18 +1100

    UR3EC: Boilerplate code

---
+++ testing_script3.m
@@ -0,0 +1,36 @@
+clear all;
+
+gg = light()
+gg.Position = [-4, 0, 2];
+
+conveyor_belt = ConveyorBelt('ConveyorBeltFixed.PLY');
+robot_ur3e = UR3EC(transl(0,0,0));
+conveyor_belt.set_transform_4by4(eye(4));
+
+a = Rubbish('HalfSizedRedGreenBrick.ply');
+b = Rubbish('HalfSizedRedGreenBrick.ply');
+c = Rubbish('HalfSizedRedGreenBrick.ply');
+
+a.attach_parent(conveyor_belt);
+a.set_transform_4by4(transl(0,0,0));
+
+b.attach_parent(conveyor_belt);
+b.set_transform_4by4(transl(0.3,-0.1,0));
+
+c.attach_parent(conveyor_belt);
+c.set_transform_4by4(transl(-0.2,0.2,0));
+
+axis equal
+view(3)
+
+rate = rateControl(20);
+for i = 1:1000
+    conveyor_belt.tick();
+    robot_ur3e.tick();
+    conveyor_belt.render();
+    robot_ur3e.render();
+    drawnow;
+    waitfor(r);
+    
+    axis equal
+end
