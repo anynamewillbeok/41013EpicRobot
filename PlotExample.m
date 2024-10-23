3e63d0d19fb2563856dfeb29e8db668e2ba46550
Kermit Nicou <14184030@student.uts.edu.au>
2024-10-23 15:46:29 +1100

    Plot with bin

--- PlotExample.m
+++ PlotExample.m
@@ -3,6 +3,7 @@ clear all;
 room_size = 10;
 conveyor_start = [-2, 5, 0.5];
 brick_offset_z = 0.5;
+bin_position = [-2.5, 6, 0];  % X, Y, Z
 
 room = RoomPlotter('concrete.jpg', 'Recycling Background.jpg', 'BackgroundRecycle.jpg');
 room.plotRoom();
@@ -13,19 +14,24 @@ gg.Position = [-4, 0, 2];
 conveyor_belt = ConveyorBelt('ConveyorBeltFixed.PLY');
 conveyor_belt.set_transform_4by4(transl(conveyor_start));
 
-%initialize and position bricks
-brick_positions = [-1.7, 4.95; -1.9, 4.85; -1.8, 5.15];  % X, Y positions of bricks
+the_bin = Rubbish('FixedBin.ply');
+rotation_matrix = trotx(pi/2);  
+bin_transform = transl(bin_position) * rotation_matrix; 
+the_bin.set_transform_4by4(bin_transform); 
+
+brick_positions = [-1.7, 4.95; -1.9, 4.85; -1.8, 5.15]; 
 bricks = initialize_bricks(brick_positions, conveyor_belt, brick_offset_z);
+
 axis equal;
 view(3);
 
 for i = 1:1000
     conveyor_belt.tick();
     conveyor_belt.render();
+    the_bin.render();
     pause(16.66 / 1000);
 end
 
-%function initialize and position bricks
 function bricks = initialize_bricks(brick_positions, conveyor_belt, offset_z)
     num_bricks = size(brick_positions, 1);
     bricks = Rubbish.empty(num_bricks, 0);
